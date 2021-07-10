3d6703c0528fbf7a0a70f9b92176c0b2a54d1fcd
piotr <nwg.piotr@gmail.com>
2021-07-10 02:36:04 +0200

    font size

--- nwg_wrapper/config/date-wttr.sh
+++ nwg_wrapper/config/date-wttr.sh
@@ -2,5 +2,5 @@
 
 time=$(LC_ALL=C TZ='Europe/Warsaw' date +'%A, %d. %B')
 wttr=$(curl https://wttr.in/?format=1)
-echo '<span size="25000" foreground="#998000">'$time'</span><span size="20000" foreground="#ccc">'
+echo '<span size="35000" foreground="#998000">'$time'</span><span size="30000" foreground="#ccc">'
 echo $wttr'</span>'
