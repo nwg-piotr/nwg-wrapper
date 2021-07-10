669a423b08892cc411121de5412567f16019eff7
piotr <nwg.piotr@gmail.com>
2021-07-10 02:32:55 +0200

    just date w/o time

--- nwg_wrapper/config/date-wttr.sh
+++ nwg_wrapper/config/date-wttr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-time=$(LC_ALL=C TZ='Europe/Warsaw' date +'%A, %d. %B  %H:%M')
+time=$(LC_ALL=C TZ='Europe/Warsaw' date +'%A, %d. %B')
 wttr=$(curl https://wttr.in/?format=1)
 echo '<span size="25000" foreground="#998000">'$time'</span><span size="20000" foreground="#ccc">'
 echo $wttr'</span>'
