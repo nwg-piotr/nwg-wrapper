b0998a2b4b69eaae5cec3d80aa16e40ea77601ca
Hill <hillyu@live.com>
2022-01-04 16:16:56 +0800

    add examples

---
+++ examples/album.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#simplay place the album in a fixed place and trigger reload of this script(using signal).
+
+echo -e "#img path=$HOME/.cache/cover.png width=280 height=280"
---
+++ examples/failedservice.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#!/usr/bin/env bash
+
+title="Failed Systemd Unit:"
+color="#c88fae"
+cmd="$(systemctl --failed)"
+textfont="Fixedsys Excelsior MonoL"
+# fontsize="6pt"
+source ~/.config/nwg-wrapper/termout.sh
+
+
